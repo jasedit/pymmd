58ed54f2211d8e88c682106de1a5cd4ba3577e48
Jason Ziglar <jpz@vt.edu>
2016-08-23 21:52:22 -0400

    Initial cut of the pymmd module.

---
+++ README.rst
@@ -0,0 +1,35 @@
+pymmd
+============
+
+Python wrapper for `MultiMarkdown <https://github.com/fletcher/MultiMarkdown-5>`, which converts MultiMarkdown flavored text into one of several outputs formats. This package directly wraps the reference implementation, and provides a simple interface to the library.
+
+The `ctypes <https://docs.python.org/2/library/ctypes.html>` package is used to wrap libMultiMarkdown in a portable fashion.
+
+Installation
+=============
+
+This package requires the
+
+Examples
+=============
+
+Converting a string of MultiMarkdown directly to various outputs:
+.. code:: python
+
+  import pymmd
+  # Generate string of MultiMarkdown text named data
+
+  html_output = pymmd.convert(data)
+  latex_output = pymmd.convert(data, fmt=pymmd.LATEX)
+
+  #Generate a snippet
+  html_snippet = pymmd.convert(data, ext=pymmd.SNIPPET)
+
+Files can also be converted directly, which enables the `Transclusion <http://fletcher.github.io/MultiMarkdown-5/transclusion>` capabilities of MultiMarkdown.
+
+.. code:: python
+  import pymmd
+
+  #MMD file named data.mmd
+
+  html_output = pymmd.convert_from("./data.mmd")
---
+++ pymmd/mmd.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import os.path
+
+import ctypes
+import ctypes.util
+
+MMDLibraryLocation = ctypes.util.find_library('MultiMarkdown')
+
+MMDLibrary = ctypes.cdll.LoadLibrary(MMDLibraryLocation)
+
+# Extension options
+COMPATIBILITY = 0
+COMPLETE = 1 << 1
+SNIPPET = 1 << 2
+HEAD_CLOSED = 1 << 3
+SMART = 1 << 4
+NOTES = 1 << 5
+NO_LABELS = 1 << 6
+FILTER_STYLES = 1 << 7
+PROCESS_HTML = 1 << 8
+NO_METADATA = 1 << 9
+OBFUSCATE = 1 << 10
+CRITIC = 1 << 11
+CRITIC_ACCEPT = 1 << 12
+CRITIC_REJECT = 1 << 13
+RANDOM_FOOT = 1 << 14
+HEADINGSECTION = 1 << 15
+ESCAPED_LINE_BREAKS = 1 << 16
+NO_STRONG = 1 << 17
+NO_EMPH = 1 << 18
+
+# Options for conversion formats for MMD
+HTML = 1
+TEXT = 2
+LATEX = 3
+MEMOIR = 4
+BEAMER = 5
+OPML = 6
+ODF = 7
+RTF = 8
+CRITIC_ACCEPT = 9
+CRITIC_REJECT = 10
+CRITIC_HTML_HIGHLIGHT = 11
+LYX = 12
+TOC = 13
+
+class GString(ctypes.Structure):
+  _fields_ = [("str", ctypes.c_char_p),
+              ("currentStringBufferSize", ctypes.c_ulong),
+              ("currentStringLength", ctypes.c_ulong)]
+
+def _expand_source(source, dname, fmt):
+  MMDLibrary.g_string_new.restype = ctypes.POINTER(GString)
+  MMDLibrary.g_string_new.argtypes = [ctypes.c_char_p]
+  src = source.encode('utf-8')
+  gstr = MMDLibrary.g_string_new(src)
+
+  MMDLibrary.prepend_mmd_header(gstr)
+  MMDLibrary.append_mmd_footer(gstr)
+
+  manifest = MMDLibrary.g_string_new(b"")
+  MMDLibrary.transclude_source.argtypes = [ctypes.POINTER(GString), ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(GString)]
+  MMDLibrary.transclude_source(gstr, dname.encode('utf-8'), None, fmt, manifest)
+  MMDLibrary.g_string_free(manifest, True)
+  full_txt = gstr.contents.str
+  MMDLibrary.g_string_free(gstr, True)
+
+  return full_txt.decode('ascii')
+
+def has_metadata(source, ext):
+  fn = MMDLibrary.has_metadata
+  fn.restype = ctypes.c_bool
+  return fn(source, ext)
+
+def convert(source, ext=COMPLETE, fmt=HTML, dname=None):
+  if dname and os.path.exists(dname):
+    source = _expand_source(source, dname, fmt)
+  MMDLibrary.markdown_to_string.argtypes = [ctypes.c_char_p, ctypes.c_ulong, ctypes.c_int]
+  MMDLibrary.markdown_to_string.restype = ctypes.c_char_p
+  src = source.encode('utf-8')
+  return MMDLibrary.markdown_to_string(src, ext, fmt).decode('ascii')
+
+def convert_from(fname, ext=COMPLETE, fmt=HTML):
+  source = open(fname, 'r').read()
+  dname = os.path.dirname(fname)
+
+  return convert(source, ext, fmt, dname)
