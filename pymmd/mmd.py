f547be9334cd50676df4116fc24bc18250710093
Jason Ziglar <jasedit@gmail.com>
2016-09-06 21:30:03 -0400

    Shift to 4 spaces per indent level

--- pymmd/mmd.py
+++ pymmd/mmd.py
@@ -47,101 +47,104 @@ LYX = 12
 TOC = 13
 
 class GString(ctypes.Structure):
-  _fields_ = [("str", ctypes.c_char_p),
-              ("currentStringBufferSize", ctypes.c_ulong),
-              ("currentStringLength", ctypes.c_ulong)]
+    _fields_ = [("str", ctypes.c_char_p),
+                ("currentStringBufferSize", ctypes.c_ulong),
+                ("currentStringLength", ctypes.c_ulong)]
 
 def _expand_source(source, dname, fmt):
-  """Expands source text to include headers, footers, and expands Multimarkdown transclusion directives.
+    """Expands source text to include headers, footers, and expands Multimarkdown transclusion
+    directives.
 
-  Keyword arguments:
-  source -- string containing the Multimarkdown text to expand
-  dname -- directory name to use as the base directory for transclusion references
-  fmt -- format flag indicating which format to use to convert transclusion statements
-  """
-  _MMD_LIB.g_string_new.restype = ctypes.POINTER(GString)
-  _MMD_LIB.g_string_new.argtypes = [ctypes.c_char_p]
-  src = source.encode('utf-8')
-  gstr = _MMD_LIB.g_string_new(src)
+    Keyword arguments:
+    source -- string containing the Multimarkdown text to expand
+    dname -- directory name to use as the base directory for transclusion references
+    fmt -- format flag indicating which format to use to convert transclusion statements
+    """
+    _MMD_LIB.g_string_new.restype = ctypes.POINTER(GString)
+    _MMD_LIB.g_string_new.argtypes = [ctypes.c_char_p]
+    src = source.encode('utf-8')
+    gstr = _MMD_LIB.g_string_new(src)
 
-  _MMD_LIB.prepend_mmd_header(gstr)
-  _MMD_LIB.append_mmd_footer(gstr)
+    _MMD_LIB.prepend_mmd_header(gstr)
+    _MMD_LIB.append_mmd_footer(gstr)
 
-  manifest = _MMD_LIB.g_string_new(b"")
-  _MMD_LIB.transclude_source.argtypes = [ctypes.POINTER(GString), ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(GString)]
-  _MMD_LIB.transclude_source(gstr, dname.encode('utf-8'), None, fmt, manifest)
-  _MMD_LIB.g_string_free(manifest, True)
-  full_txt = gstr.contents.str
-  _MMD_LIB.g_string_free(gstr, True)
+    manifest = _MMD_LIB.g_string_new(b"")
+    _MMD_LIB.transclude_source.argtypes = [ctypes.POINTER(GString), ctypes.c_char_p,
+                                           ctypes.c_char_p, ctypes.c_int, ctypes.POINTER(GString)]
+    _MMD_LIB.transclude_source(gstr, dname.encode('utf-8'), None, fmt, manifest)
+    _MMD_LIB.g_string_free(manifest, True)
+    full_txt = gstr.contents.str
+    _MMD_LIB.g_string_free(gstr, True)
 
-  return full_txt.decode('ascii')
+    return full_txt.decode('ascii')
 
 def has_metadata(source, ext):
-  """Returns a flag indicating if a given block of MultiMarkdown text contains metadata."""
-  fn = _MMD_LIB.has_metadata
-  fn.restype = ctypes.c_bool
-  return fn(source, ext)
+    """Returns a flag indicating if a given block of MultiMarkdown text contains metadata."""
+    fn = _MMD_LIB.has_metadata
+    fn.restype = ctypes.c_bool
+    return fn(source, ext)
 
 def convert(source, ext=COMPLETE, fmt=HTML, dname=None):
-  """Converts a string of MultiMarkdown text to the requested format.
-  Transclusion is performed if the COMPATIBILITY extension is not set, and dname is set to a valid directory
+    """Converts a string of MultiMarkdown text to the requested format.
+    Transclusion is performed if the COMPATIBILITY extension is not set, and dname is set to a
+    valid directory
 
-  Keyword arguments:
-  source -- string containing MultiMarkdown text
-  ext -- extension bitfield to pass to conversion process
-  fmt -- flag indicating output format to use
-  dname -- directory to use for transclusion - if None, transclusion functionality is bypassed
-  """
-  if dname and os.path.exists(dname) and not (ext & COMPATIBILITY):
-    source = _expand_source(source, dname, fmt)
-  _MMD_LIB.markdown_to_string.argtypes = [ctypes.c_char_p, ctypes.c_ulong, ctypes.c_int]
-  _MMD_LIB.markdown_to_string.restype = ctypes.c_char_p
-  src = source.encode('utf-8')
-  return _MMD_LIB.markdown_to_string(src, ext, fmt).decode('ascii')
+    Keyword arguments:
+    source -- string containing MultiMarkdown text
+    ext -- extension bitfield to pass to conversion process
+    fmt -- flag indicating output format to use
+    dname -- directory to use for transclusion - if None, transclusion functionality is bypassed
+    """
+    if dname and os.path.exists(dname) and not (ext & COMPATIBILITY):
+        source = _expand_source(source, dname, fmt)
+    _MMD_LIB.markdown_to_string.argtypes = [ctypes.c_char_p, ctypes.c_ulong, ctypes.c_int]
+    _MMD_LIB.markdown_to_string.restype = ctypes.c_char_p
+    src = source.encode('utf-8')
+    return _MMD_LIB.markdown_to_string(src, ext, fmt).decode('ascii')
 
 def convert_from(fname, ext=COMPLETE, fmt=HTML):
-  """Converts a file containing MultiMarkdown text to the requested format.
-  Transclusion is performed if the COMPATIBILITY extension is not set.
+    """Converts a file containing MultiMarkdown text to the requested format.
+    Transclusion is performed if the COMPATIBILITY extension is not set.
 
-  Keyword arguments:
-  fname -- string containing MultiMarkdown text
-  ext -- extension bitfield to pass to conversion process
-  fmt -- flag indicating output format to use
-  """
-  source = open(fname, 'r').read()
-  dname = os.path.dirname(fname)
+    Keyword arguments:
+    fname -- string containing MultiMarkdown text
+    ext -- extension bitfield to pass to conversion process
+    fmt -- flag indicating output format to use
+    """
+    source = open(fname, 'r').read()
+    dname = os.path.dirname(fname)
 
-  return convert(source, ext, fmt, dname)
+    return convert(source, ext, fmt, dname)
 
 def extract_metadata_keys(source, ext=COMPLETE):
-  """Extracts metadata keys from the provided MultiMarkdown text.
+    """Extracts metadata keys from the provided MultiMarkdown text.
 
-  Keyword arguments:
-  source -- string containing MultiMarkdown text
-  ext -- extension bitfield for extracting MultiMarkdown
-  """
-  _MMD_LIB.extract_metadata_keys.restype = ctypes.c_char_p
-  _MMD_LIB.extract_metadata_keys.argtypes = [ctypes.c_char_p, ctypes.c_ulong]
-  src = source.encode('utf-8')
-  return _MMD_LIB.extract_metadata_keys(src, ext).decode('ascii')
+    Keyword arguments:
+    source -- string containing MultiMarkdown text
+    ext -- extension bitfield for extracting MultiMarkdown
+    """
+    _MMD_LIB.extract_metadata_keys.restype = ctypes.c_char_p
+    _MMD_LIB.extract_metadata_keys.argtypes = [ctypes.c_char_p, ctypes.c_ulong]
+    src = source.encode('utf-8')
+    return _MMD_LIB.extract_metadata_keys(src, ext).decode('ascii')
 
 def extract_metadata_value(source, ext, key):
-  """ Extracts value for the specified metadata key from the given extension set.
+    """ Extracts value for the specified metadata key from the given extension set.
 
-  Keyword arguments:
-  source -- string containing MultiMarkdown text
-  ext -- extension bitfield for processing text
-  key -- key to extract
-  """
-  _MMD_LIB.extract_metadata_value.restype = ctypes.c_char_p
-  _MMD_LIB.extract_metadata_value.argtypes = [ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p]
+    Keyword arguments:
+    source -- string containing MultiMarkdown text
+    ext -- extension bitfield for processing text
+    key -- key to extract
+    """
+    _MMD_LIB.extract_metadata_value.restype = ctypes.c_char_p
+    _MMD_LIB.extract_metadata_value.argtypes = [ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p]
 
-  src = source.decode('utf-8')
-  dkey = key.decode('utf-8')
+    src = source.decode('utf-8')
+    dkey = key.decode('utf-8')
 
-  return _MMD_LIB.extract_metadata_value(src, ext, dkey).decode('ascii')
+    return _MMD_LIB.extract_metadata_value(src, ext, dkey).decode('ascii')
 
 def version():
-  """Returns a string containing the MultiMarkdown library version in use."""
-  _MMD_LIB.mmd_version.restype = ctypes.c_char_p
-  return _MMD_LIB.mmd_version()
+    """Returns a string containing the MultiMarkdown library version in use."""
+    _MMD_LIB.mmd_version.restype = ctypes.c_char_p
+    return _MMD_LIB.mmd_version()
