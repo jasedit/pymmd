5c67cf8889b36b6cb0e40b45db92c9215283db33
Jason Ziglar <jasedit@gmail.com>
2016-09-24 15:05:15 -0400

    Fix library path computation

--- pymmd/download.py
+++ pymmd/download.py
@@ -12,9 +12,9 @@ except ImportError:
     from urllib import urlopen
 
 SHLIB_PREFIX = {
-  'Linux': '',
-  'Darwin': '',
-  'Windows': 'Release/'
+  'Linux': '.',
+  'Darwin': '.',
+  'Windows': 'Release'
 }
 
 SHLIB_EXT = {
@@ -54,9 +54,9 @@ def build_mmd(target_folder):
     subprocess.call(['cmake', '-DCMAKE_BUILD_TYPE=Release', '-DSHAREDBUILD=1', '..'])
     PLATFORM_BUILDS[platform.system()]()
 
-    lib_file = SHLIB_PREFIX[platform.system()] + 'libMultiMarkdown' + SHLIB_EXT[platform.system()]
+    lib_file = 'libMultiMarkdown' + SHLIB_EXT[platform.system()]
     if not os.path.exists(target_folder):
       os.mkdir(target_folder)
-    shutil.copyfile(os.path.join(build_dir, lib_file), os.path.join(target_folder, lib_file))
+    shutil.copyfile(os.path.join(build_dir, SHLIB_PREFIX[platform.system()], lib_file), os.path.join(target_folder, lib_file))
     os.chdir(old_pwd)
     shutil.rmtree(mmd_dir)
