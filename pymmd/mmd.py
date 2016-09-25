82d4749cf0bd5b8b66a626f517129b4dd76d63ce
Jason Ziglar <jasedit@gmail.com>
2016-09-25 10:12:48 -0400

    Remove *_from to simplify interface

--- pymmd/mmd.py
+++ pymmd/mmd.py
@@ -105,10 +105,6 @@ def has_metadata(source, ext):
     _MMD_LIB.has_metadata.restype = ctypes.c_bool
     return _MMD_LIB.has_metadata(source.encode('utf-8'), ext)
 
-def has_metadata_from(fname, ext):
-    with open(fname, 'r') as ifp:
-        return has_metadata(ifp.read(), ext)
-
 def convert(source, ext=COMPLETE, fmt=HTML, dname=None):
     """Converts a string of MultiMarkdown text to the requested format.
     Transclusion is performed if the COMPATIBILITY extension is not set, and dname is set to a
@@ -127,36 +123,10 @@ def convert(source, ext=COMPLETE, fmt=HTML, dname=None):
     src = source.encode('utf-8')
     return _MMD_LIB.markdown_to_string(src, ext, fmt).decode('utf-8')
 
-def convert_from(fname, ext=COMPLETE, fmt=HTML, oname=None):
-    """Converts a file containing MultiMarkdown text to the requested format.
-    Transclusion is performed if the COMPATIBILITY extension is not set.
-
-    Keyword arguments:
-    fname -- string containing MultiMarkdown text
-    ext -- extension bitfield to pass to conversion process
-    fmt -- flag indicating output format to use
-    oname -- optional filename to write MMD output to disk
-    """
-    source = open(fname, 'r').read()
-    dname = os.path.dirname(fname) or '.'
-
-    mmd = convert(source, ext, fmt, dname)
-
-    if oname:
-        with open(oname, 'w') as ofp:
-            ofp.write(mmd)
-    return mmd
-
 def manifest(txt, dname):
     """Extracts file manifest for a body of text with the given directory."""
     _, files = _expand_source(txt, dname, HTML)
     return files
-
-def manifest_from(fname):
-    """Extracts the file manifest for a given document."""
-    source = open(fname, 'r').read()
-    dname = os.path.dirname(fname) or '.'
-    return manifest(source, dname)
 
 def extract_metadata_keys(source, ext=COMPLETE):
     """Extracts metadata keys from the provided MultiMarkdown text.
@@ -171,11 +141,6 @@ def extract_metadata_keys(source, ext=COMPLETE):
     keys = _MMD_LIB.extract_metadata_keys(src, ext).decode('utf-8')
     keys = [ii for ii in keys.split('\n') if ii]
     return keys
-
-def extract_metadata_keys_from(fname, ext=COMPLETE):
-    """Extracts metadata keys from the provided file."""
-    txt = open(fname, 'r').read()
-    return extract_metadata_keys(txt, ext)
 
 def extract_metadata_value(source, key, ext=COMPLETE):
     """ Extracts value for the specified metadata key from the given extension set.
@@ -192,10 +157,6 @@ def extract_metadata_value(source, key, ext=COMPLETE):
     dkey = key.encode('utf-8')
 
     return _MMD_LIB.extract_metadata_value(src, ext, dkey).decode('utf-8')
-
-def extract_metadata_value_from(fname, key, ext=COMPLETE):
-    with open(fname, 'r') as ifp:
-        return extract_metadata_value(ifp.read(), key, ext)
 
 def version():
     """Returns a string containing the MultiMarkdown library version in use."""
